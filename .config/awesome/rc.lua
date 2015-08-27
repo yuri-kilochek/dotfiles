f94381a95e46fd0de36e9f4f17c6fecb6b3848e0
Yuri Kilochek <yuri.kilochek@gmail.com>
2015-08-27 23:21:57 +0300

    Implemented custom theme.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -15,16 +15,13 @@ do
     end)
 end
 
+local beautiful = require("beautiful")
+beautiful.init("~/.config/awesome/theme.lua")
+
 local gears = require("gears")
 local awful = require("awful")
 awful.rules = require("awful.rules")
 local wibox = require("wibox")
-local beautiful = require("beautiful")
-
--- Variable definitions
--- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
-beautiful.border_width = 3
 
 ALT = "Mod1"
 META = "Mod4"
@@ -69,19 +66,30 @@ client_keys = awful.util.table.join(
     end),
 {})
 
+local tag_focus = {}
+
 for i = 1, #awful.tag.gettags(1) do
     root_keys = awful.util.table.join(root_keys,
         awful.key({ META }, tostring(i), function()
-            local t = awful.tag.gettags(1)[i]
-            awful.tag.viewonly(t)
+            local old = awful.tag.selected(1)
+            tag_focus[old] = client.focus
+
+            local new = awful.tag.gettags(1)[i]
+            awful.tag.viewonly(new)
+            if tag_focus[new] ~= nil then
+                client.focus, tag_focus[new] = tag_focus[new], nil
+            end
         end),
     {})
     client_keys = awful.util.table.join(client_keys,
         awful.key({ META, CTRL }, tostring(i), function(c)
-            local t = awful.tag.gettags(1)[i]
-            awful.tag.viewonly(t)
-            awful.client.movetotag(t, c)
-            client.focus = c
+            local old = awful.tag.selected(1)
+            tag_focus[old] = old:clients()[1]
+
+            local new = awful.tag.gettags(1)[i]
+            awful.tag.viewonly(new)
+            awful.client.movetotag(new, c)
+            client.focus, tag_focus[new] = c, nil
         end),
     {})
 end
@@ -120,7 +128,7 @@ awful.rules.rules = {
         rule = {},
         properties = {
             size_hints_honor = false,
-            border_color = beautiful.border_normal,
+            border_color = beautiful.border_color,
             focus = awful.client.focus.filter,
             raise = true,
             keys = client_keys,
@@ -128,24 +136,6 @@ awful.rules.rules = {
         },
     },
 }
-
-do
-    local tag_focus = {}
-    tag.connect_signal('property::selected', function(t)
-        if t.selected then
-            if tag_focus[t] then
-                client.focus = tag_focus[t]
-            else
-                local tcs = t:clients()
-                if #tcs > 0 then
-                    client.focus = tcs[1]
-                end
-            end
-        else
-            tag_focus[t] = client.focus
-        end
-    end)
-end
 
 client.connect_signal('tagged', function(c, t)
     local tcs = t:clients()
@@ -164,25 +154,30 @@ client.connect_signal('untagged', function(c, t)
     if #tcs == 1 then
         tcs[1].border_width = 0
     end
-    --[[
-    local has_focused = false;
-    for _, cc in ipairs(tcs) do
-        if cc == client.focus then
-            has_focused = true
-            break
+
+    for _, c in ipairs(tcs) do
+        if c == client.focus then
+            return
         end
     end
-    if not has_focused and #tcs > 0 then
-        client.focus = tcs[1]
+    
+    -- focus previous client in focus history that is in this tag
+    for i = 0, #client.get(1) - 1 do
+        local c = awful.client.focus.history.get(1, i)
+        for _, cc in ipairs(tcs) do
+            if cc == c then
+                client.focus = c
+                return
+            end
+        end
     end
-    --]]
 end)
 
-client.connect_signal("focus", function(c)
-    c.border_color = beautiful.border_focus
+client.connect_signal("focus", function(client)
+    client.border_color = beautiful.border_focus_color
 end)
 
-client.connect_signal("unfocus", function(c)
-    c.border_color = beautiful.border_normal
+client.connect_signal("unfocus", function(client)
+    client.border_color = beautiful.border_color
 end)
 
---
+++ .config/awesome/theme.lua
@@ -0,0 +1,12 @@
+local theme = {}
+
+theme.font = "sans 8"
+
+theme.background_color = "#111111"
+theme.foreground_color = "#eeeeee"
+
+theme.border_width = 2 
+theme.border_color = "#002e3c"
+theme.border_focus_color = "#009cc9"
+
+return theme
