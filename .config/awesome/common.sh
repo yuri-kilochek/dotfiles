7e91c26a1301b73c52fd42db2c76cf0feee7e69f
Yuri Kilochek <yuri.kilochek@gmail.com>
2015-09-11 15:47:37 +0300

    Refactored more stuff, added bars.

--- .config/awesome/common.sh
+++ .config/awesome/common.sh
@@ -6,60 +6,70 @@ hide_cursor() {
     tput civis
 }
 
-move_top_left() {
-    tput cup 0 0
-}
-
 get_shape() (
-    lines=0
-    columns=0
-    IFS=''
-    while read -r line; do
-        lines=$(expr $lines + 1)
-        if [ ${#line} -gt $columns ]; then
-            columns=${#line}
-        fi
-    done 
-    printf '%d %d' $lines $columns
+    if [ $# -eq 1 ]; then
+        echo "$1" | get_shape
+    else
+        lines=0
+        columns=0
+        IFS=''
+        while read -r line; do
+            lines=$(expr $lines + 1)
+            if [ ${#line} -gt $columns ]; then
+                columns=${#line}
+            fi
+        done 
+        printf '%d %d' $lines $columns
+    fi
 )
 
 reshape() {
     printf $'\e[8;%d;%dt' $1 $2
 }
 
-reshape_and_print() {
-    reshape $(echo "$1" | get_shape)
+render() {
+    reshape $(get_shape "$1")
+    tput cup 0 0
+    tput ed
     printf '%s' "$1"
 }
 
 read_key() (
     IFS=''
-    read -rsn1 key
+    if [ $# -eq 1 ]; then
+        read -t "$1" -rsn1 key
+    else
+        read -rsn1 key
+    fi
     read -t 0.0001 -rsn1 k && key="$key$k"
     read -t 0.0001 -rsn1 k && key="$key$k"
     read -t 0.0001 -rsn1 k && key="$key$k"
     read -t 0.0001 -rsn1 k && key="$key$k"
     read -t 0.0001 -rsn1 k && key="$key$k" 
-    echo "$key"
+    printf '%s' "$key"
 )
 
 bar() (
     width="$1"
-    value="$2"
-    state="$3"
-    percent=$(printf '%.0f%%' "$(echo "$value * 100" | bc -l)")
-    if [ "$state" = 'o' ]; then
-        segment='#'
-    elif [ "$state" = 'x' ]; then
-        segment='='
+    tokens="$2"
+    percent="$3"
+    part=$(printf '%.0f' "$(echo $percent / 100 '*' $width | bc -l)")
+    for _ in $(seq "$part"); do
+        printf '%s' "${tokens:0:1}"
+    done
+    for _ in $(seq "$(expr "$width" "-" "$part")"); do
+        printf '%s' "${tokens:1:1}"
+    done
+)
+
+trialign() (
+    width=$1
+    left=$2
+    middle=$3
+    right=$4
+    gap=$(expr \( $width - ${#middle} \) / 2)
+    if [ $(expr $gap '*' 2 + ${#middle}) -lt $width ]; then
+        middle=" $middle"
     fi
-    progress=$(printf '%.0f' "$(echo "$value * $width" | bc -l)")
-    printf '%4s [' "$percent" 
-    for _ in $(seq "$progress"); do
-        printf "$segment"
-    done
-    for _ in $(seq "$(expr "$width" "-" "$progress")"); do
-        printf ' '
-    done
-    printf ']'
+    printf '%-*s%s%*s' $gap "$left" "$middle" $gap "$right"
 )
