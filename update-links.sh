2b7c22c31ca193c6836c71e5611bffee15f193ec
Yuri Kilochek <yuri.kilochek@gmail.com>
2018-10-31 10:21:25 -0200

    More robust link updater.

--- update-links.sh
+++ update-links.sh
@@ -2,8 +2,10 @@
 
 dir=$(dirname "$0")
 file=$(basename "$0")
-for f in $(find $dir -type f -and -not -path "$dir/.git/**" -and -not -name $file); do
-    f=${f#$(echo $dir/)}
-    ln -f $f ~/$f
+for f in $(find "$dir" -type f -and -not -path "$dir/.git/**" -and -not -name "$file"); do
+    f=${f#"$dir/"}
+    d="$(dirname "$f")"
+    mkdir -p ~/"$d"
+    ln -f "$f" ~/"$f"
 done
 
