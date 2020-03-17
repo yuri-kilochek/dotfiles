3d49efceb41181dc24d309773c03e9367ce23e1e
Yuriy Kilochek <ykilochek@networkoptix.com>
2020-03-17 13:55:25 +0300

    Don't mess with non-interactive shell, it breaks `scp`.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,36 +5,38 @@ if status --is-login
     end
 end
 
-function __drop_prompt
-    for i in (seq 100)
-        echo
+if status --is-interactive
+    function __drop_prompt
+        for i in (seq 100)
+            echo
+        end
     end
-end
 
-function clear --wraps=clear
-    command clear
+    function clear --wraps=clear
+        command clear
+        __drop_prompt
+    end
+
     __drop_prompt
-end
 
-__drop_prompt
+    function fish_prompt
+        set -l last_status $status
+        
+        echo
+        echo '[' $USER@(hostname) ']' (prompt_pwd)
+        echo '> '
 
-function fish_prompt
-    set -l last_status $status
-    
-    echo
-    echo '[' $USER@(hostname) ']' (prompt_pwd)
-    echo '> '
+        return $last_status
+    end
 
-    return $last_status
-end
+    function __clear_statusline --on-event fish_preexec
+        set -l last_status $status
+        
+        tput cuu1
+        tput cuu1
+        tput dl1
+        tput cud1
 
-function __clear_statusline --on-event fish_preexec
-    set -l last_status $status
-    
-    tput cuu1
-    tput cuu1
-    tput dl1
-    tput cud1
-
-    return $last_status
+        return $last_status
+    end
 end
